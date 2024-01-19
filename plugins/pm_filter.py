b659ca93881cc7a9b91bdad292c3337a2934cb1f
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-19 23:29:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1423,7 +1423,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]]
             log_msg = await client.send_cached_media(
                 chat_id=GENERAT,
-                file_id="kf",
+                file_id=kf,
                 parse_mode=enums.ParseMode.HTML,                                
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
