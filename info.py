d21ce94808ae72549dfed85e54b15b1681e6fbda
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-19 14:35:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -136,8 +136,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://azanwebsite.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://azanwebsite.onrender.com/".format(FQDN, PORT)
+URL = "https://minnalmuralioldbot.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://minnalmuralioldbot.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -152,9 +152,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://azanwebsite.onrender.com/".format(FQDN)
+    URL = "https://minnalmuralioldbot.onrender.com/".format(FQDN)
 else:
-    URL = "https://azanwebsite.onrender.com/".format(FQDN)
+    URL = "https://minnalmuralioldbot.onrender.com/".format(FQDN)
 
 # UPLOAD_CHANNEL = environ.get('UPLOAD_CHANNEL',"https://t.me/batchfiles_store")
 #redict
