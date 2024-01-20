c16c4b4c25eae83a92fe4c6d6d1162970ba0210f
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-20 18:40:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1408,6 +1408,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await m.delete()
             await asyncio.sleep(600)
             await k.delete()
+           
+
     elif query.data.startswith("uploaded"):
        
         conten = query.message.reply_to_message.text
@@ -1487,37 +1489,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             sticker=sticker,            
             reply_markup=reply_markup,                       
             )
-            users = await db.get_all_users()
-        
-            sts = await query.message.reply_text(
-            text='Broadcasting your messages...'
-            )
-            start_time = time.time()
-            total_users = await db.total_users_count()
-            done = 0
-            blocked = 0
-            deleted = 0
-            failed =0
-
-            success = 0
-            async for user in users:
-                pti, sh = await broadcast_messages(int(user['id']), sp)
-                if pti:
-                    success += 1
-                elif pti == False:
-                    if sh == "Blocked":
-                        blocked+=1
-                    elif sh == "Deleted":
-                        deleted += 1
-                    elif sh == "Error":
-                        failed += 1
-                done += 1
-                await asyncio.sleep(2)
-                if not done % 20:
-                    await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-            time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-            await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-                   
+                               
 
     
     elif query.data.startswith("already_available"):
