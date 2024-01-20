9f8dda44cc4213107abd0e965c610edc2dcc53b5
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-20 15:14:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1378,9 +1378,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         user_id = query.from_user.id
         username =  query.from_user.mention
 #        try:
-        try:
-            if int(query.from_user.id) not in [query.message.reply_to_message.from_user.id, 0]:
-#            if not userid:
+        try:            
+            if not userid:
                 return await query.answer(
                     f"⚠️ ʜᴇʟʟᴏ{query.from_user.first_name},\nᴛʜɪꜱ ɪꜱ ɴᴏᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ,\nʀᴇQᴜᴇꜱᴛ ʏᴏᴜʀ'ꜱ...",
                     show_alert=True,
