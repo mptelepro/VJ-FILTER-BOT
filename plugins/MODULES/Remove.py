88cec69397dc65478bc2570ced83e9e72c92d0c5
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 03:20:09 +0530

    Update Remove.py

--- plugins/MODULES/Remove.py
+++ plugins/MODULES/Remove.py
@@ -4,18 +4,18 @@ import telebot
 import requests
 
 # Ganti 'TOKEN_BOT_ANDA' dengan token bot Telegram Anda
-TOKEN = '2108094040:AAHY_MkFF5X5HhW4yaZzq49jduK2fySPlhM'
-bot = telebot.TeleBot(TOKEN)
+BOT_TOKEN = '2108094040:AAHY_MkFF5X5HhW4yaZzq49jduK2fySPlhM'
+bot = telebot.TeleBot(BOT_TOKEN)
 
 # Ganti 'TOKEN_REMOVEBG_API' dengan token API Remove.bg Anda
 REMOVEBG_API_KEY = 'MJMoiiatXPHcHgFG3D1Wf2aG'
 
-@bot.message_handler(commands=['game'])
+@bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     try:
         file_id = message.photo[-1].file_id
         file_info = bot.get_file(file_id)
-        file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_info.file_path}"
+        file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
 
         # Kirim permintaan ke Remove.bg API untuk menghapus background gambar
         response = requests.post('https://api.remove.bg/v1.0/removebg', data={'image_url': file_url}, headers={'X-Api-Key': REMOVEBG_API_KEY})
@@ -29,6 +29,6 @@ def handle_photo(message):
             bot.reply_to(message, "Maaf, tidak dapat menghapus background gambar.")
 
     except Exception as e:
-        bot.reply_to(message, "Terjadi kesalahan saat memproses gambar.")
+        bot.reply_to(message, "An error occurred while processing the image..")
 
 bot.polling()
