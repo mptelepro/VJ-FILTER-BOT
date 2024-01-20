b04503a11de342b50d7351297ab6d92020eb1dc1
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-20 18:19:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1500,29 +1500,23 @@ async def cb_handler(client: Client, query: CallbackQuery):
             failed =0
 
             success = 0
-            b_msg = sp
             async for user in users:
-                   
-                pti, sh = await broadcast_messages(int(user['id']), sp)
-                if pti:
-                    success += 1
-                elif pti == False:
-                    if sh == "Blocked":
-                        blocked+=1
-                    elif sh == "Deleted":
-                        deleted += 1
-                    elif sh == "Error":
-                        failed += 1
-                done += 1
-                await asyncio.sleep(2)
-                if not done % 20:
-                    
-                    sp = await sts.edit(f"Broadcast Completed:\nCompleted in  seconds.\n\nTotal Users {total_users}\nCompleted:")
-                    
-                    await m.delete()
-                    await sp.delete()
-                    await asyncio.sleep(600)
-                    await k.delete()
+            pti, sh = await broadcast_messages(int(user['id']), sp)
+            if pti:
+                success += 1
+            elif pti == False:
+                if sh == "Blocked":
+                    blocked+=1
+                elif sh == "Deleted":
+                    deleted += 1
+                elif sh == "Error":
+                    failed += 1
+            done += 1
+            await asyncio.sleep(2)
+            if not done % 20:
+                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+        time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+        await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
                    
 
     
