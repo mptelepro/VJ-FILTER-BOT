442c7cc81dcfee85529a24bd0295a3bb8d2b3550
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 13:13:52 +0530

    Update Remove.py

--- plugins/MODULES/Remove.py
+++ plugins/MODULES/Remove.py
@@ -20,24 +20,23 @@ REMOVEBG_API_KEY = 'MJMoiiatXPHcHgFG3D1Wf2aG'
 @bot.message_handler(commands=["reverse"])
 def handle_photo(message):
     try:
-        if message.reply_to_message.content_type == "photo":
-            file_id = message.photo[-1].file_id
-            file_info = bot.get_file(file_id)
-            file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
+        file_id = message.photo[-1].file_id
+        file_info = bot.get_file(file_id)
+        file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
 
-            # Kirim permintaan ke Remove.bg API untuk menghapus background gambar
-            response = requests.post('https://api.remove.bg/v1.0/removebg', data={'image_url': file_url}, headers={'X-Api-Key': REMOVEBG_API_KEY})
+        # Kirim permintaan ke Remove.bg API untuk menghapus background gambar
+        response = requests.post('https://api.remove.bg/v1.0/removebg', data={'image_url': file_url}, headers={'X-Api-Key': REMOVEBG_API_KEY})
 
-            if response.status_code == 200:
-                # Simpan gambar hasil dan kirimkan ke pengguna
-                with open('removed_bg.png', 'wb') as f:
-                    f.write(response.content)
-                bot.send_document(message.chat.id, open('removed_bg.png', 'rb'))
-            else:
-                bot.reply_to(message, "Maaf, tidak dapat menghapus background gambar.")
-    
-        except Exception as e:
-            bot.reply_to(message, "An error occurred while processing the image..")
+        if response.status_code == 200:
+            # Simpan gambar hasil dan kirimkan ke pengguna
+            with open('removed_bg.png', 'wb') as f:
+                f.write(response.content)
+            bot.send_document(message.chat.id, open('removed_bg.png', 'rb'))
+        else:
+            bot.reply_to(message, "Maaf, tidak dapat menghapus background gambar.")
+
+    except Exception as e:
+        bot.reply_to(message, "An error occurred while processing the image..")
 
 
 
@@ -64,7 +63,7 @@ def handle_photo(message):
     except Exception as e:
         bot.reply_to(message, "An error occurred while processing the image..")
 
-bot.polling()
+
 
 
 bot.polling()
