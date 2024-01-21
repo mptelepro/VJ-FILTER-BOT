3a5dd578a1b1d92eecf949b67ebcd7eb8b209a05
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 18:25:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1969,50 +1969,24 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "sinfo":
         await query.answer(text=script.SINFO, show_alert=True)
 
-   
-
-    elif query.data == "okdaa":
-        await client.send_message(
-            chat_id=message.chat.id,
-            text="Select your required mode from below!ㅤㅤ",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(text="𝖡𝗋𝗂𝗀𝗍𝗁", callback_data="bright"),
-                        InlineKeyboardButton(text="𝖬𝗂𝗑𝖾𝖽", callback_data="mix"),
-                        InlineKeyboardButton(text="𝖡 & 𝖶", callback_data="b|w"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="𝖢𝗂𝗋𝖼𝗅𝖾", callback_data="circle"),
-                        InlineKeyboardButton(text="𝖡𝗅𝗎𝗋", callback_data="blur"),
-                        InlineKeyboardButton(text="𝖡𝗈𝗋𝖽𝖾𝗋", callback_data="border"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="𝖲𝗍𝗂𝖼𝗄𝖾𝗋", callback_data="stick"),
-                        InlineKeyboardButton(text="𝖱𝗈𝗍𝖺𝗍𝖾", callback_data="rotate"),
-                        InlineKeyboardButton(text="𝖢𝗈𝗇𝗍𝗋𝖺𝗌𝗍", callback_data="contrast"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="𝖲𝖾𝗉𝗂𝖺", callback_data="sepia"),
-                        InlineKeyboardButton(text="𝖯𝖾𝗇𝖼𝗂𝗅", callback_data="pencil"),
-                        InlineKeyboardButton(text="𝖢𝖺𝗋𝗍𝗈𝗈𝗇", callback_data="cartoon"),
-                    ],
-                    [
-                        InlineKeyboardButton(text="𝖨𝗇𝗏𝖾𝗋𝗍", callback_data="inverted"),
-                        InlineKeyboardButton(text="𝖦𝗅𝗂𝗍𝖼𝗁", callback_data="glitch"),
-                        InlineKeyboardButton(
-                            text="𝖱𝖾𝗆𝗈𝗏𝖾 𝖡𝖦", callback_data="removebg"
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(text="𝖢𝗅𝗈𝗌𝖾", callback_data="close_data"),
-                    ],
-                ]
-            ),
-            reply_to_message_id=message.id,        
-        )
-       
-
+    elif query.data == "removebg":
+       await query.message.edit_text(
+           text="**Select required mode**",
+           reply_markup=InlineKeyboardMarkup( [[
+               InlineKeyboardButton(text="𝖶𝗂𝗍𝗁 𝖶𝗁𝗂𝗍𝖾 𝖡𝖦", callback_data="rmbgwhite"),
+               InlineKeyboardButton(text="𝖶𝗂𝗍𝗁𝗈𝗎𝗍 𝖡𝖦", callback_data="rmbgplain"),
+               ],[
+               InlineKeyboardButton(text="𝖲𝗍𝗂𝖼𝗄𝖾𝗋", callback_data="rmbgsticker"),
+               ],[
+               InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='photo')
+               ]]
+           )
+       )
+    
+    elif query.data == "rmbgwhite":
+        await removebg_white(client, query.message)
+    elif query.data == "rmbgplain":
+        await removebg_plain(client, query.message)
     
     elif query.data == "start":
         grp_id = query.message.chat.id
