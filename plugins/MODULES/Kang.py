3c7c7367c0f227b7f8caf04f902f712bd0e88be6
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 20:27:01 +0530

    Update Kang.py

--- plugins/MODULES/Kang.py
+++ plugins/MODULES/Kang.py
@@ -9,7 +9,7 @@ from plugins.helpers.stickerset import *
     
 MAX_STICKERS = (120)  # would be better if we could fetch this limit directly from telegram
 SUPPORTED_TYPES = ["jpeg", "png", "webp"]
-BOT_USERNAME 
+# BOT_USERNAME 
 
 @Client.on_message(filters.command("get_sticker"))
 async def sticker_image(_, message: Message):
