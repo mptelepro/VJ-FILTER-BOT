7381be4c3f7ef7ce9530c41fe2a5b30b8b9d42d3
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 14:42:04 +0530

    Update testbg.py

--- plugins/MODULES/testbg.py
+++ plugins/MODULES/testbg.py
@@ -18,21 +18,21 @@ REMOVEBG_API_KEY = 'MJMoiiatXPHcHgFG3D1Wf2aG'
 @Client.on_message(filters.command(["test"]))
 async def remove_background(client, message):
     try:
-        if message.reply_to_message.photo:
-            file_id = message.reply_to_message.photo[-1].file_id
-            file_info = bot.get_file(file_id)
-            file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
+#        if message.reply_to_message.photo:
+        file_id = message.reply_to_message.photo[-1].file_id
+        file_info = bot.get_file(file_id)
+        file_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
 
-            # Kirim permintaan ke Remove.bg API untuk menghapus background gambar
-            response = requests.post('https://api.remove.bg/v1.0/removebg', data={'image_url': file_url}, headers={'X-Api-Key': REMOVEBG_API_KEY})
+        # Kirim permintaan ke Remove.bg API untuk menghapus background gambar
+        response = requests.post('https://api.remove.bg/v1.0/removebg', data={'image_url': file_url}, headers={'X-Api-Key': REMOVEBG_API_KEY})
 
-            if response.status_code == 200:
-                 # Simpan gambar hasil dan kirimkan ke pengguna
-                with open('removed_bg.png', 'wb') as f:
-                    f.write(response.content)
-                await client.send_document(open('removed_bg.png', 'rb'), quote=True)
-            else:
-               await client.send_message("Maaf, tidak dapat menghapus background gambar.")
+        if response.status_code == 200:
+                # Simpan gambar hasil dan kirimkan ke pengguna
+            with open('removed_bg.png', 'wb') as f:
+                f.write(response.content)
+            await message.reply_document(open('removed_bg.png', 'rb'), quote=True)
+        else:
+            await client.send_message("Maaf, tidak dapat menghapus background gambar.")
 
     except Exception as e:
         await client.send_message("An error occurred while processing the image..")
