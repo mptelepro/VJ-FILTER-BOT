03c01ee4dcc7b3c4a1c09238f90f4f3af88cc356
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 21:59:11 +0530

    Update carbon.py

--- plugins/MODULES/carbon.py
+++ plugins/MODULES/carbon.py
@@ -1,26 +1,37 @@
-from io import BytesIO
-from aiohttp import ClientSession
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-aiohttpsession = ClientSession()
+from pyrogram.types import *
+from aiohttp import ClientSession
+from telegraph import upload_file
+from io import BytesIO
+
+ai_client = ClientSession()
+
+async def make_carbon(code, tele=False):
+    url = "https://carbonara.solopov.dev/api/cook"
+    async with ai_client.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    if tele:
+        uf = upload_file(image)
+        image.close()
+        return f"https://graph.org{uf[0]}"
+    return image
 
 
 @Client.on_message(filters.command("carbon"))
-async def carbon_func(_, message):
+async def carbon_func(b, message):
     if not message.reply_to_message:
-        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")        
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")
     if not message.reply_to_message.text:
-        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")       
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ.")
     user_id = message.from_user.id
-    code = message.reply_to_message.text
     m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
-    url = "https://carbonara.vercel.app/api/cook"
-    async with aiohttpsession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())
-    image.name = "carbon.png"   
+    carbon = await make_carbon(message.reply_to_message.text)
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
-    await message.reply_photo(photo=image, caption="**MADE WITH ❤️ BY >JEOL & MKN**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("JOIN CHANNEL", url="https://t.me/beta_boTZ")]])                 
+    await message.reply_photo(
+        photo=carbon,
+        caption="**ᴍᴀᴅᴇ ʙʏ: @mkn_bots_updates**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/mkn_bots_updates")]]),                   
     )
     await m.delete()
     carbon.close()
