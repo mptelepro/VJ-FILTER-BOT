4576a66a6f76509c91beea2780eb3f1d8ddd9389
Azanwebsite <156943940+Azanwebsite@users.noreply.github.com>
2024-01-21 16:25:49 +0530

    Update testbg.py

--- plugins/MODULES/testbg.py
+++ plugins/MODULES/testbg.py
@@ -24,7 +24,7 @@ from info import *
 
 
 load_dotenv()
-BOT_TOKEN = '5894671404:AAGCQIb0moTV0n34hLZ0dCBERituyY_deIU'
+BOT_TOKEN = '2108094040:AAHY_MkFF5X5HhW4yaZzq49jduK2fySPlhM'
 bot = Client(BOT_TOKEN)
 
 # Ganti 'TOKEN_REMOVEBG_API' dengan token API Remove.bg Anda
